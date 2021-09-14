f5cfb3bdc10037bf05856975d0f9e9418c42d36b
paployanjr <tigranp@boomte.ch>
2021-09-14 12:34:01 +0400

    BF-461 Single Choice Field

--- src/Fields/SingleChoice/SingleChoice.js
+++ src/Fields/SingleChoice/SingleChoice.js
@@ -1,29 +1,74 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef } from 'react'
 import classNames from 'classnames/bind'
-import { Radio } from 'boomform'
+import Quantity from '../Quantity/Quantity'
+import { Radio, Input } from 'boomform'
 
-const SingleChoice = ({ id, label, options, classnameprefix, ...props }) => {
+const SingleChoice = ({
+  id,
+  label,
+  options,
+  classnameprefix,
+  quantity,
+  ...props
+}) => {
+  const { value: quantityValue, label: quantityLabel, enabled } = quantity
+  const otherRef = useRef(null)
   return (
     <>
-      {options.map(({ key, label, value, checked }) => {
-        return (
-          <label
-            className={classNames('boomForm-singleChoice__item', {
-              [`${classnameprefix}-singleChoice__item`]: classnameprefix
-            })}
-            key={`${id}.${key}`}
-          >
-            <span>{label}</span>
-            <Radio
-              {...props}
-              id={`${id}.${key}`}
-              name={id}
-              value={value}
-              initial={checked ? true : false}
-            />
-          </label>
-        )
+      {options.map(({ key, label, placeholder, value, checked }) => {
+        if (key === 'other')
+          return (
+            <label
+              ref={otherRef}
+              className={classNames('boomForm-singleChoice__item', {
+                [`${classnameprefix}-singleChoice__item`]: classnameprefix
+              })}
+              key={`${id}.${key}`}
+            >
+              <Radio
+                {...props}
+                id={`${id}.${key}`}
+                name={id}
+                value={value || null}
+                initial={checked}
+              />
+              <Input
+                {...props}
+                id={`${id}.otherValue`}
+                type='text'
+                placeholder={placeholder}
+                onBlur={() => otherRef.current.click()}
+              />
+            </label>
+          )
+        else
+          return (
+            <label
+              className={classNames('boomForm-singleChoice__item', {
+                [`${classnameprefix}-singleChoice__item`]: classnameprefix
+              })}
+              key={`${id}.${key}`}
+            >
+              <Radio
+                {...props}
+                id={`${id}.${key}`}
+                name={id}
+                value={value}
+                initial={checked}
+              />
+              <span>{label}</span>
+            </label>
+          )
       })}
+
+      {enabled && (
+        <Quantity
+          id={id}
+          label={quantityLabel}
+          value={quantityValue}
+          classnameprefix={classnameprefix}
+        />
+      )}
     </>
   )
 }
