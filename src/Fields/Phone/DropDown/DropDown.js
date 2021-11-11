45542a48c12b59644c1078417d026229a7f61499
Rafayel Sahakyan <rafayels@bommte.ch>
2021-11-11 14:50:57 +0400

    Use classnames.

--- src/Fields/Phone/DropDown/DropDown.js
+++ src/Fields/Phone/DropDown/DropDown.js
@@ -9,7 +9,7 @@ import {
 } from '../../../Helpers/phone'
 
 const DropDown = ({ id, defaultCountryCode }) => {
-  const [isOpen, setIsOpen] = useState('none')
+  const [isOpen, setIsOpen] = useState('false')
 
   let [defaultCountry] = countryListForPhone
   if (defaultCountryCode)
@@ -17,7 +17,7 @@ const DropDown = ({ id, defaultCountryCode }) => {
 
   const handleClickClose = (e) => {
     if (e.target.classList.value.indexOf('country_code') === -1)
-      setIsOpen('none')
+      setIsOpen('false')
   }
 
   useEffect(() => {
@@ -29,7 +29,7 @@ const DropDown = ({ id, defaultCountryCode }) => {
   return (
     <div
       className={classNames('country_code_picker', {
-        country_code_picker_active: isOpen !== 'none'
+        country_code_picker_active: isOpen !== 'false'
       })}
     >
       <Custom initial={defaultCountry.dial_code} id={`${id}.code`}>
@@ -40,13 +40,13 @@ const DropDown = ({ id, defaultCountryCode }) => {
             <>
               <div
                 className='country_code_action'
-                onClick={(event) => setIsOpen(isOpen === 'none' ? event.target?.offsetTop < 200 ? 'bottom' : 'top' : 'none')}
+                onClick={(event) => setIsOpen(isOpen === 'false' ? event.target?.offsetTop < 200 ? 'bottom' : 'top' : 'false')}
               >
                 <img src={flag} alt={flag} className='country_code_image' />
               </div>
               <div>{value}</div>
-              {isOpen !== 'none' && (
-                <div className={`country_code_dropdown open_${isOpen}`}>
+              {isOpen !== 'false' && (
+                <div className={classNames('country_code_dropdown', {open_bottom: isOpen === 'bottom'})}>
                   <div className='country_code_search'>
                     <Input
                       id={`${id}.search`}
