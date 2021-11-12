f22ae2344360abb7a49401ccd957c1f12dab0a51
Rafayel Sahakyan <rafayels@bommte.ch>
2021-11-12 18:32:35 +0400

    Fixed textSplit variable.

--- src/Fields/Terms/Terms.js
+++ src/Fields/Terms/Terms.js
@@ -2,20 +2,27 @@ import React, { Fragment } from 'react'
 import { Checkbox } from 'boomform'
 
 const Terms = ({ id, text, url, openIn, ...props }) => {
-  const textSplit = text.match(/(?<part1>.*){(?<linkText>.*)}(?<part2>.*)/).groups
+  const textSplit = text.match(
+    /(?<part1>.*){(?<linkText>.*)}(?<part2>.*)/
+  )?.groups
   const targetValue = openIn === 'newTab' ? '_blank' : 'popup'
-  const newTab = targetValue === "popup"
-  
+  const newTab = targetValue === 'popup'
+
   return (
     <>
       <Checkbox id={id} name={id} value={id} {...props} />
       <div className='boomForm-terms__link'>
-        {textSplit.part1}
-        <a href={url}
+        {textSplit?.part1 || ''}
+        <a
+          href={url}
           target={targetValue}
-          onClick={() => newTab && window.open(url, 'Popup', 'width = 800, height = 600')}
-        >{textSplit.linkText}</a>
-        {textSplit.part2}
+          onClick={() =>
+            newTab && window.open(url, 'Popup', 'width = 800, height = 600')
+          }
+        >
+          {textSplit?.linkText || ''}
+        </a>
+        {textSplit?.part2 || ''}
       </div>
     </>
   )
