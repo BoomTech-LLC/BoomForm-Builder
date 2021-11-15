d3055ce4d2eb8812ca5254413f48fad16fd9bf26
Rafayel Sahakyan <rafayels@bommte.ch>
2021-11-15 17:40:03 +0400

    Added tag for show and hide password.

--- src/Fields/Password/Password.js
+++ src/Fields/Password/Password.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { Input } from 'boomform'
 
 const Password = ({ label, classnameprefix, ...props }) => {
-  return <Input {...props} type='password' />
+  const [showPassword, setShowPassword] = useState(false);
+  return <> 
+    <Input {...props} type={showPassword ? 'text' : 'password'} />
+    <span onClick={() => setShowPassword(value => !value)} >
+    <before content='gegs' style={{width: '50px', height: '50px', backgroundColor: '#000', color: "#fff"}}></before>
+    </span>
+  </>
 }
 
 export default Password
