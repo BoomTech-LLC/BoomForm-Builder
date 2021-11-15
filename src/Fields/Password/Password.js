b537d4e3cc8c45559e6454f91a4a453e293ce8e8
Rafayel Sahakyan <rafayels@bommte.ch>
2021-11-15 18:49:39 +0400

    Added toggle for classname.npm

--- src/Fields/Password/Password.js
+++ src/Fields/Password/Password.js
@@ -1,11 +1,15 @@
 import React, { Fragment, useState } from 'react'
+import classNames from 'classnames/bind'
 import { Input } from 'boomform'
 
 const Password = ({ label, classnameprefix, ...props }) => {
   const [showPassword, setShowPassword] = useState(false);
   return <> 
     <Input {...props} type={showPassword ? 'text' : 'password'} />
-    <span className='show-password'></span>
+    <span 
+      onClick={() => setShowPassword(value => !value)}  
+      className={classNames({'no-show-password': !showPassword, 'show-password': showPassword})}
+    ></span>
   </>
 }
 
