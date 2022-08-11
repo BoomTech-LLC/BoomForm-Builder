f8d4ec9f5b20a20fc3f6bdf97805071bde95ac36
Tigran <tigrannazaryan99@gmail.com>
2022-08-11 11:45:42 +0400

    Update Boomform Builder

--- src/Body/Fields.js
+++ src/Body/Fields.js
@@ -12,15 +12,22 @@ const Fields = ({
   updatableFields,
   payment
 }) => {
+  console.log(1, updatableFields)
+
   const data = useField(updatableFields)
+
+  console.log(2, data)
 
   const logicIds = getHiddenIds({
     logic,
     values: data?.neededValues ? data?.neededValues : {},
     fields
   })
+
+  console.log(3, logicIds)
   const printableFields = getPrintableFields(fields, logicIds, pagination)
 
+  console.log(4, printableFields)
   if (printableFields.length === 0) setCurrentPage((prev) => prev + 1)
 
   return (
