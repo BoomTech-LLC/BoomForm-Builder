f6f5013ef50c3288a8a6b2700f176a35c35b0cd0
Amram1997 <amramananyan9@gmail.com>
2023-01-30 17:58:10 +0400

    delete imports

--- src/Body/Fields/File/File.js
+++ src/Body/Fields/File/File.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { uploadFiles, correctFiles, ABORT_REQUEST_CONTROLLERS } from './../../../Helpers/files'
 import List from './List'
 import { Input } from 'boomform'
