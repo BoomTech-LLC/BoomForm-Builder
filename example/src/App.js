9d1f00d5a1fc9c092f0d6555112f6a1cc6eeceb5
Tigran0199 <cruelman11@gmail.com>
2023-10-31 09:25:27 +0400

    feat: Added gridOptions property on example

--- example/src/App.js
+++ example/src/App.js
@@ -1,141 +1,99 @@
 import React from 'react'
 import { Builder } from 'boomform-builder'
-
-const App = () => {
-  // const singleField = {
-  //   buttons: { type: 0, next: 'Next', prev: 'Prev' },
-  //   initial: 0,
-  //   pagesCounter: true,
-  //   pages: [
-  //     { title: '', description: '', fields: ['storeButton', 'text'] },
-  //     { title: '', description: '', fields: ['storeButton', 'password'] },
-  //     { title: '', description: '', fields: ['storeButton', 'date'] },
-  //     { title: '', description: '', fields: ['storeButton', 'email'] }
-  //   ]
-  // }
-
-  return (
-    <Builder
-      global={{
-        name: 'Name',
-        description: 'Decription',
-        isPrint: true
-      }}
-      fields={[
-        {
-          type: 'textarea',
-          id: 3,
-          height: '60px',
-          placeholder: 'Your Comments',
-          label: 'Your Comments',
-          validation: {
-            HTMLValidate: true,
-            min: {
-              msg: 'Please enter a value between {0} and {1}.',
-              value: null,
-              type: 'length'
+        
+const App = () => { 
+    return (
+        <Builder
+          global={{
+            name: 'Name',
+            description: 'Decription',
+            isPrint: true
+          }}
+          fields={[
+            {
+              type: 'text',
+              id: 1,
+              placeholder: 'Single Line',
+              label: 'Single Line Field',
             },
-            max: {
-              msg: 'Please enter a value between {0} and {1}.',
-              value: null,
-              type: 'length'
+            {
+              type: 'text',
+              id: 2,
+              placeholder: 'Single Line',
+              label: 'Single Line Field',
+            },
+            {
+              type: 'textarea',
+              id: 3,
+              placeholder: 'Enter your long text',
+              label: 'Multi line field',
+            },
+            {
+              type: 'textarea',
+              id: 4,
+              placeholder: 'Your Comments',
+              label: 'Left a comment',
             }
-          }
-        },
-        {
-          type: 'file',
-          id: 10,
-          isMultiple: true,
-          inputContent: '',
-          upLoadData: {
-            url: 'https://storage.apiboomtech.com/file',
-            headers: {
-              Authorization:
-                'Bearer sl.BZX4eUyGRmCoRR3lAjokvL5UB6Y5sV_q8BT--zff_WTRwUa_xkIe38R6DxgN_Lqqb5KtIf5GvoiIGi2ml6ZPPSXAX7unkglCKrNysIVFt8shT3pXl9JbdUXyMSTi_92RFZmr8pI',
-              'Content-Type': 'multipart/form-data',
-              'Access-Control-Allow-Origin': '*'
-            },
-            queries: {
-              platform: 'form',
-              folder: 'wix'
-            },
-            onPostSuccess: (responce) => {
-              console.log(
-                'File uploaded successfully  and got responce :',
-                responce
-              )
-            },
-            onPostFail: (status, error) => {
-              console.log('Posting status is : ', status, 'error : ', error)
-            }
-          },
-
-          label: 'Upload Your File ',
-          validation: {
-            HTMLValidate: true
-          }
-        },
-        {
-          type: 'textarea',
-          id: 55,
-          height: '600px',
-          placeholder: 'Your Comments',
-          label: 'Your Comments',
-          validation: {
-            HTMLValidate: true,
-            min: {
-              msg: 'Please enter a value between {0} and {1}.',
-              value: null,
-              type: 'length'
-            },
-            max: {
-              msg: 'Please enter a value between {0} and {1}.',
-              value: null,
-              type: 'length'
-            }
-          }
-        }
-      ]}
-      // logic={[
-      //   {
-      //     id: 'date',
-      //     action: 'show',
-      //     operator: 'and',
-      //     conditions: [
-      //       {
-      //         id: 'password',
-      //         value: 'a',
-      //         rule: 'is'
-      //       }
-      //     ]
-      //   }
-      // ]}
-      // pagination={{
-      //   buttons: { type: 0, next: "Next", prev: "Prev" },
-      //   initial: 0,
-      //   pages: [{
-      //     title: 'Page 1',
-      //     description: "",
-      //     fields:['storeButton','text','signature','password']
-      //   }, {
-      //     title: 'Page 2',
-      //     description: "",
-      //     fields:['storeButton','phone','name']
-      //     },
-      //     {
-      //       title: 'Page 3',
-      //       description: "",
-      //       fields:['storeButton','phone','password']
-      //     }
-      //   ],
-      //   timeline:1,
-      // }}
-      // pagination={singleField}
-      button={{
-        text: 'submit'
-      }}
-    />
-  )
+          ]}
+          button={{
+            text: 'submit'
+          }}
+          // gridOptions={
+          //   {
+          //    cols: 4,
+          //    width: 800,
+          //    rowHeight: 1,
+          //    margin: [0, 0],
+          //    isBounded: false,
+          //    isDraggable: false,
+          //    isResizable: false,
+          //    containerPadding: [0, 0],
+          //    layout: {
+          //        1: {
+          //          h: 119,
+          //          maxH: 119,
+          //          maxW: 4,
+          //          minH: 119,
+          //          minW: 1,
+          //          w: 2,
+          //          x: 0,
+          //          y: 0
+          //        },
+          //        2: {
+          //          h: 119,
+          //          maxH: 119,
+          //          maxW: 4,
+          //          minH: 119,
+          //          minW: 1,
+          //          w: 2,
+          //          x: 2,
+          //          y: 0
+          //        },
+          //        3: {
+          //          h: 110,
+          //          maxH: 110,
+          //          maxW: 4,
+          //          minH: 110,
+          //          minW: 2,
+          //          w: 4,
+          //          x: 0,
+          //          y: 238
+          //        },
+          //        4: {
+          //          h: 119,
+          //          maxH: 119,
+          //          maxW: 4,
+          //          minH: 119,
+          //          minW: 1,
+          //          w: 3,
+          //          x: 0,
+          //          y: 0
+          //        }
+          //      }
+          //   }
+          // }
+        />
+    )
 }
-
+        
 export default App
