f18dfe9f32bb9df00d4e4c1de3338d9da84c777c
HrachBeqejyan777 <hrachb@boomte.ch>
2023-11-29 18:25:00 +0400

    add placeholders prop

--- src/Helpers/time.js
+++ src/Helpers/time.js
@@ -1,8 +1,8 @@
-export const getTimeFields = (format) => {
+export const getTimeFields = (format, placeholders) => {
   const fields = [
-    { key: 'hour', placeholder: 'HH' },
-    { key: 'minute', placeholder: 'MM' },
+    { key: 'hour', placeholder: placeholders?.hour || 'HH' },
+    { key: 'minute', placeholder: placeholders?.minutes || 'MM' },
     { key: 'format' }
-  ]
-  return format === '24h' ? fields.splice(0, 2) : fields
-}
+  ];
+  return format === '24h' ? fields.splice(0, 2) : fields;
+};
