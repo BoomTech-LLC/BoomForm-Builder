7546c8bdee6d09429e6cfd3b8b30eb08b0f3eafb
Tigran0199 <cruelman11@gmail.com>
2024-03-09 00:10:50 +0400

    feat: Added quantity in item

--- src/Body/Fields/SingleChoice/Item/index.js
+++ src/Body/Fields/SingleChoice/Item/index.js
@@ -2,10 +2,18 @@ import React, { Fragment } from 'react'
 import { Radio } from 'boomform'
 import classNames from 'classnames'
 import { formatPrice } from './../../../../Helpers/payment'
+import Quantity from '../../Quantity/Quantity'
 
-const Item = ({ id, option, payment, classnameprefix, validation }) => {
-  const { key, label, value, checked, price } = option
-
+const Item = ({
+  id,
+  option,
+  payment,
+  classnameprefix,
+  validation,
+  quantity,
+  isPerOption
+}) => {
+  const { key, label, value, checked, price, quantity: optionQuantity } = option
   return (
     <label
       className={classNames('boomForm-singleChoice__item', {
@@ -24,6 +32,11 @@ const Item = ({ id, option, payment, classnameprefix, validation }) => {
             })
         }}
       ></span>
+      {isPerOption && <Quantity
+        id={`${id}.${key}`}
+        classnameprefix={classnameprefix}
+        quantity={optionQuantity || quantity}
+      />}
     </label>
   )
 }
