2388ce131e0f5e6d5d37dd942e829866c011cb8b
GAnanikyan <gagik.ananikyan.y@gmail.com>
2024-03-25 13:48:27 +0400

    restore example

--- example/src/App.js
+++ example/src/App.js
@@ -1,33 +1,15 @@
-import React from 'react';
-// import Sidebar from './Sidebar'
-// import Content from './Content'
-import { Builder } from 'boomform-builder';
+import React, { useState } from 'react'
+import Sidebar from './Sidebar'
+import Content from './Content'
 
 const App = () => {
-  // const [example, setExample] = useState({ name: 'Logic', option: 'logic' })
+  const [example, setExample] = useState({ name: 'Logic', option: 'logic' })
   return (
     <>
-      <Builder
-        fields={[
-          {
-            id: 'email',
-            type: 'email',
-            label: 'Please Type Your Email',
-            placeholder: 'Type ...',
-            validation: {
-              email: { msg: 'Wrong Email Format' },
-              required:{msg:"SUKI SUIKI"},
-              regEx: {
-                msg: 'Business emails only, common providers like Gmail, Yahoo, etc., are not allowed.',
-                value:
-                  '^$|^[a-zA-Z0-9._%+-]+@(?!yahoo\.com|hotmail\.com|outlook\.com|aol\.com|icloud\.com|protonmail\.com|mail\.com|zoho\.com|live\.com|msn\.com|ymail\.com|gmx\.com|fastmail\.com|mail\.ru|bk\.ru|gmail\.com)[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-              }
-            }
-          }
-        ]}
-      />
+      <Sidebar example={example} setExample={setExample} />
+      <Content example={example} />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
