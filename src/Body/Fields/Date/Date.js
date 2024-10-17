6f842d475de9b8623755591106280f4b9f08df89
Anzhela <sahakyananzhela@gmail.com>
2024-10-17 17:56:26 +0400

    PF-323 added validation for disabled custom dates

--- src/Body/Fields/Date/Date.js
+++ src/Body/Fields/Date/Date.js
@@ -1,92 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Input } from 'boomform'
 import { iphoneCheck } from '../../../Helpers/global'
+import moment from 'moment'
 
 const Date = ({ validation = {}, payment, ...props }) => {
-  const { min, max } = validation
-  const [selectedDate, setSelectedDate] = useState(null)
+  const { min, max, hideDays, disableDates, hiddenCustomDays, isCustom } =
+    validation
 
-  if (min || max) {
+  if (min || max || (hideDays && disableDates)) {
     validation = {
       ...validation,
-      custom: value => {
+      custom: (value) => {
         if (value) {
+          // const dayName = getDayName(value)
+          const dayName = moment(value).format('dddd')
+          const customDay = moment(value).format('MMM D, YYYY')
           if (min?.value > value) return min?.msg
           if (max?.value < value) return max?.msg
+          // if (hideDays?.monday && value == '2024-10-21')
+          //   return 'pick nomrallll date'
+          if (hideDays[dayName] && !isCustom) {
+            return `${dayName}s are disabled. Please pick another date. `
+          }
+          if (hiddenCustomDays.includes(value) && isCustom) {
+            return `${customDay} is disabled. Please pick another date.`
+          }
         }
         return false
       }
     }
   }
-  let date
-  // const validate = () => {
-  //   if (hiddenDays.length > 0) {
-  //     validation = {
-  //       ...validation,
-  //       custom: () => {
-  //         console.log('custom worked')
-
-  //         return customValidationMessage || 'Selected date is not selectable'
-  //       }
-  //     }
-  //   }
-  // }
-  useEffect(() => {
-    date = new Date(selectedDate)
-  }, [selectedDate])
-
-  const hiddenDays = Object.keys(validation).filter(
-    key => key.startsWith('hide') && validation[key]?.msg
-  )
-
-  const customValidationMessage = hiddenDays
-    .map(day => validation[day].msg)
-    .join(', ')
 
   const handleChange = ({ event }) => {
-    const value = event.target.value
-    setSelectedDate(value)
-    // validate()
     if (iphoneCheck()) {
       setTimeout(() => {
         event.target.defaultValue = ''
       }, 100)
-    }
-  }
-  const hiddenDaysArray = []
-  useEffect(() => {
-    if (selectedDate) {
-      const date = new Date(selectedDate)
-      const currentDay = date.getDay()
-
-      hiddenDaysArray.forEach(({ details }) => {
-        const hiddenDayValue = details.value
-
-        if (hiddenDayValue === currentDay) {
-          validation = {
-            ...validation,
-            custom: () => {
-              return (
-                customValidationMessage || 'Selected date is not selectable'
-              )
-            }
-          }
-        }
-      })
-    }
-  }, [selectedDate, hiddenDaysArray])
-  for (const [key, value] of Object.entries(validation)) {
-    if (key.startsWith('hide') && typeof value === 'object') {
-      hiddenDaysArray.push({ day: key.replace('hide', ''), details: value })
-    }
-  }
-
-  if (hiddenDays.length > 0) {
-    validation = {
-      ...validation,
-      custom: () => {
-        return customValidationMessage || 'Selected date is not selectable'
-      }
     }
   }
 
